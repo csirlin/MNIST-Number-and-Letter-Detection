fa9f6c3f195a22a8a50f5d49e8e03eb9ee019d36
Colter Sirlin <coltersirlin@Colters-MBP.bishops.com>
2019-08-21 14:31:45 -0700

    Fixed typo

--- load_display_save.py
+++ load_display_save.py
@@ -3,6 +3,9 @@ import argparse
 import cv2
 
 ap = argparse.ArgumentParser()
-ad.add_argument("-i", "--image", required = True,
+ap.add_argument("-i", "--image", required = True,
     help = "Path to the image")
 args = vars(ap.parse_args())
+
+image = args["image"]
+print(image)
