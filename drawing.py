15c2119e1adcd64bdc053768f9d2514faa387fee
Colter Sirlin <coltersirlin@Colters-MBP.bishops.com>
2019-08-23 12:40:41 -0700

    Chaptr 5 with a twist

---
+++ drawing.py
@@ -0,0 +1,48 @@
+import numpy as np
+import cv2
+import time
+
+
+#setup
+canvas = np.zeros((300, 300, 3), dtype = "uint8")
+red = (0, 0, 255)
+green = (0, 255, 0)
+blue = (255, 0, 0)
+white = (255, 255, 255)
+
+#shapes
+"""cv2.line(canvas, (0, 0), (300, 300), green)
+
+cv2.line(canvas, (300, 0), (0, 300), red, 3)
+cv2.rectangle(canvas, (10, 10), (60, 60), green)
+cv2.rectangle(canvas, (50, 200), (200, 225), red, 5)
+cv2.rectangle(canvas, (200, 50), (225, 125), blue, -1)"""
+
+#window
+"""cv2.rectangle(canvas, (0,0), (300,300), green, 10)
+cv2.line(canvas, (150,0), (150,300), green, 5)
+cv2.line(canvas, (0,150), (300,150), green, 5)"""
+
+#circles
+"""cv2.circle(canvas, (150, 150), 150, blue, -1)
+cv2.circle(canvas, (150, 150), 125, white, -1)
+cv2.circle(canvas, (150, 150), 100, red, -1)
+cv2.circle(canvas, (150, 150), 75, blue, -1)
+cv2.circle(canvas, (150, 150), 50, white, -1)
+cv2.circle(canvas, (150, 150), 25, red, -1)"""
+
+#randomcircles-automatic
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
+for i in range(0, 1000):
+
+    radius = np.random.randint(5, high = 50)
+    color = np.random.randint(0, high = 256, size = (3,)).tolist()
+    pt = np.random.randint(0, high = 300, size = (2,))
+    cv2.circle(canvas, tuple(pt), radius, color, -1)
+    cv2.imshow("Canvas", canvas)
+    cv2.waitKey(10)
+
+#drawing
+cv2.imshow("Canvas", canvas)
+cv2.waitKey(0)
